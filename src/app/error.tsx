55d79a97ff9cb81ab425dc25154074abcc302473
MaxCode012 <seremet.maxim94@gmail.com>
2024-07-04 17:10:55 +0300

    solved client error

---
+++ src/app/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error() {
+  return <div>An error has occured. Please try again later.</div>;
+}
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-});
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? client;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "development") globalForPrisma.prisma = client;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
