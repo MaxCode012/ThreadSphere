e9bcc093aa3aef6bba2754203e47d4232f1bcdda
MaxCode012 <seremet.maxim94@gmail.com>
2024-07-14 16:05:17 +0300

    improved UX by managing ProfileCard depending on the return of getUser

--- src/components/OptionsContainer.tsx
+++ src/components/OptionsContainer.tsx
@@ -11,17 +11,11 @@ export default function OptionsContainer({
   return (
     <div
       ref={dropdownRef}
-      className="absolute lg:right-[-40px] right-0 mt-36 w-48 dark:bg-container bg-white shadow-lg rounded-lg py-2 z-50"
+      className="absolute lg:right-[-40px] right-0 mt-36 lg:mt-28 w-48 dark:bg-container bg-white shadow-lg rounded-lg py-2 z-50"
     >
       <button className="block w-full text-left px-4 py-2 dark:text-white text-black dark:hover:bg-hover hover:bg-gray-200">
         <ThemeSwitch />
       </button>
-      <Link
-        href="/signin"
-        className="block px-4 py-2 dark:text-white text-black dark:hover:bg-hover hover:bg-gray-200"
-      >
-        Sign in
-      </Link>
     </div>
   );
 }
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -2,29 +2,50 @@ import Image from "next/image";
 import MainButton from "./MainButton";
 import LogoutButton from "./LogOutButton";
 import { getUser } from "@/lib/lucia";
+import Link from "next/link";
 
 export default async function ProfileCard() {
   const user = await getUser();
 
   return (
-    <aside className="md:w-[250px] w-full mt-2 border dark:border-none dark:bg-container rounded-lg sticky top-0 h-fit space-y-2 px-1">
-      <div className="mt-2">
-        <Image
-          className="m-auto"
-          src="/no_avatarimg.png"
-          width={45}
-          height={45}
-          alt="no avatar"
-        />
-      </div>
-      <div className="space-y-7">
-        <div className="flex flex-col justify-center items-center">
-          <h2>{user?.name}</h2>
-          <p className="text-sm text-gray-400">{user?.email}</p>
+    <aside
+      className={`md:w-[250px] w-full mt-2 ${
+        !user ? "pt-3" : "pt-0"
+      } border dark:border-none dark:bg-container rounded-lg sticky top-0 h-fit space-y-2 px-1`}
+    >
+      {user && (
+        <div className="mt-2">
+          <Image
+            className="m-auto"
+            src="/no_avatarimg.png"
+            width={45}
+            height={45}
+            alt="no avatar"
+          />
         </div>
+      )}
+      <div className="space-y-7">
+        {user && (
+          <div className="flex flex-col justify-center items-center">
+            <h2>{user ? user.name : "No name"}</h2>
+            <p className="text-sm text-gray-400">
+              {user ? user.email : "No email"}
+            </p>
+          </div>
+        )}
         <div className="space-y-3 pb-3">
-          <MainButton className="w-full">Edit profile</MainButton>
-          <LogoutButton />
+          {user ? (
+            <>
+              <MainButton className="w-full">Edit profile</MainButton>
+              <LogoutButton />
+            </>
+          ) : (
+            <MainButton className="w-full px-0">
+              <Link className="w-full block text-center" href="/signin">
+                Sign in
+              </Link>
+            </MainButton>
+          )}
         </div>
       </div>
     </aside>
